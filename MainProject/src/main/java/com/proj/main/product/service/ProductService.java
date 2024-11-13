2ff55471cd9666332029deb623e4c97e721189e5
302-1 <302-1@302-16>
2024-11-13 13:00:15 +0900

    sy

--- MainProject/src/main/java/com/proj/main/product/service/ProductService.java
+++ MainProject/src/main/java/com/proj/main/product/service/ProductService.java
@@ -1,8 +1,23 @@
 package com.proj.main.product.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.proj.main.product.dao.IProductDAO;
+import com.proj.main.product.dto.ProductDTO;
 
 @Service
 public class ProductService {
-
+	
+	
+	@Autowired
+	IProductDAO dao;
+	
+	
+	public List<ProductDTO> productList(String category){
+		List<ProductDTO> result = dao.productList(category);
+		return result;
+	}
 }
